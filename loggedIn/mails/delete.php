3275cf7dd4f72d28ad13fbe50a1d5558c3a63dc5
Max Pesch <maxo.pech@gmail.com>
2015-09-27 15:59:12 +0200

    Imporved mailing system and added a trash can

--- loggedIn/mails/delete.php
+++ loggedIn/mails/delete.php
@@ -12,7 +12,7 @@ if(!isset($mid) || !intval($mid) || $mid < 0){
     Header('Location: read.php');
 }
 if(MailManager\Overview::userHas($_SESSION['studentId'], $mid)){
-    $suc = safeQuery("DELETE FROM user__messages WHERE id = $mid");
+    $suc = safeQuery("UPDATE user__messages SET deleted = NOW() WHERE id = $mid");
     Logger::log('The message (id: ' . $mid . ') was deleted', Logger::SOCIAL);
     if(!$suc){
 	echo 'Message couldn\'t be deleted';
