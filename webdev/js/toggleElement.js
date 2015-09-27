174bd2b0a2fd22e1d449be3bfb1259bb620239ba
Max Pesch <maxo.pech@gmail.com>
2015-09-27 15:55:26 +0200

    Fixed the toogleing of the cards on the overview section

--- webdev/js/toggleElement.js
+++ webdev/js/toggleElement.js
@@ -1,16 +1,16 @@
+var allContent = Array();
+
 function toggleVisibility(element) {
     allDivs = document.getElementsByTagName("div");
-    for(var i = 0; i < allDivs.length; i++){
-	if(allDivs[i] == element){
-	    if(allDivs[i].className == "card"){
+    for (var i = 0; i < allDivs.length; i++) {
+	if (allDivs[i] == element) {
+	    if (allDivs[i].className == "card") {
 		allDivs[i].className = "colapsed";
 		textElement = allDivs[i].getElementsByTagName("p")[0];
-		allDivs[i].removeChild(textElement);
-	    }else{
+		allContent[i] = allDivs[i].removeChild(textElement);
+	    } else {
 		allDivs[i].className = "card";
-		var newElement = document.createElement("p");
-		newElement.appendChild(document.createTextNode("Sometext"));
-		allDivs[i].appendChild(newElement);
+		allDivs[i].appendChild(allContent[i]);
 	    }
 	}
     }
