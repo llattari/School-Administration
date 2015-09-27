563dc2cebcd3925bb1d686433b5ec8b65411c57a
Max Pesch <maxo.pech@gmail.com>
2015-09-27 15:58:28 +0200

    Added more functions to the essentials libaray

--- webdev/php/essentials/essentials.php
+++ webdev/php/essentials/essentials.php
@@ -20,20 +20,72 @@ function isIn($search, $string) {
  * @return boolean
  */
 function check($mail = NULL, $tel = NULL, $zip = NULL) {
-    if($mail != NULL){
-	if(!filter_var($mail, FILTER_VALIDATE_EMAIL)){
+    if ($mail != NULL) {
+	if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
 	    return false;
 	}
     }
-    if($tel != NULL){
-	if(preg_match('/(\d{4}[ -\/]?)?\d+/is', $tel) == 0){
+    if ($tel != NULL) {
+	if (preg_match('/(\d{4}[ -\/]?)?\d+/is', $tel) == 0) {
 	    return false;
 	}
     }
-    if($zip != NULL){
-	if(preg_match('/\d+/is', $zip) == 0){
+    if ($zip != NULL) {
+	if (preg_match('/\d+/is', $zip) == 0) {
 	    return false;
 	}
+    }
+    return true;
+}
+
+/**
+ * validId($var)
+ * @param int $var
+ * @return boolean
+ */
+function validId($var) {
+    return $var > 0;
+}
+
+/**
+ * roundUp($value, $decimalPlaces)
+ *  Rounds a number up to a certain accuracy
+ * @param float $value
+ * 	The value that should be rounded
+ * @param int $decimalPlaces
+ * 	Amount of digits after the decimal point
+ * @return float
+ * 	Returns a float or an int if $decimalPlaces = 0
+ */
+function roundUp($value, $decimalPlaces = 0) {
+    $digit = (int) $decimalPlaces;
+    return ceil($value * pow(10, $digit)) / pow(10, $digit);
+}
+
+/**
+ * roundDown($value, $decimalPlaces)
+ *  Rounds a number down to a certain accuracy
+ * @param float $value
+ * 	The value that should be rounded
+ * @param int $decimalPlaces
+ * 	Amount of digits after the decimal point
+ * @return float
+ * 	Returns a float or an int if $decimalPlaces = 0
+ */
+function roundDown($value, $decimalPlaces = 0) {
+    $digit = (int) $decimalPlaces;
+    return floor($value * pow(10, $digit)) / pow(10, $digit);
+}
+
+/**
+ * isDate($date)
+ * 	Checks if a string is a valid date
+ * @param string $date
+ * @return boolean
+ */
+function isDate($date) {
+    if (!isset($date) && strtotime($date) === false) {
+	return false;
     }
     return true;
 }
