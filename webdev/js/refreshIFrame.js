6a65f8f0459c374a0babeb408f7d4386d8ad26f9
max <max@max-linux-mint>
2015-12-14 06:44:18 +0100

    Improved CSS Styles and edited javascript to also relaod the online user IFrame

--- webdev/js/refreshIFrame.js
+++ webdev/js/refreshIFrame.js
@@ -1,9 +1,11 @@
 function reloadChat() {
-    var iframe = document.getElementsByTagName('iframe')[0];
-    iframe.contentWindow.location.reload();
-    console.log('Test');
+    var iframes = document.getElementsByTagName('iframe');
+    for (var i = 0; i < iframes.length; i++) {
+	iframes[i].contentWindow.location.reload();
+    }
 }
 
 function startChat() {
     setInterval(reloadChat, 2000);
+    console.log('Sucessfully keeping up with chat and online user');
 }
